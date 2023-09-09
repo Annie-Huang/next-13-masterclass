6081dae5c9177a8e7d31fcc699665395aba96b50
Annie Huang <annie.hn@gmail.com>
2023-09-09 13:49:20 +1000

    Call supabase.auth.signUp method in handleSubmit

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,12 +1,25 @@
 'use client';
 
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+
 import AuthForm from '@/app/(auth)/AuthForm';
 
 export default function Signup() {
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
 
-    console.log('user signup', email, password);
+    // console.log('user signup', email, password);
+
+    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#client-side
+    // location.origin is the browser address the user currently is on.
+    const supabase = createClientComponentClient();
+    await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`,
+      },
+    });
   };
 
   return (
