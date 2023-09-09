ce86845329c3f3092ccb6d7d55295ede09cd02db
Annie Huang <annie.hn@gmail.com>
2023-09-09 15:17:45 +1000

    Get supbase from cookies and exchange the code if the code is in url

--- app/api/auth/callback/route.js
+++ app/api/auth/callback/route.js
@@ -1,4 +1,18 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
 export async function GET(request) {
   const url = new URL(request.url);
   const code = url.searchParams.get('code');
+
+  if (code) {
+    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#route-handlers
+    // https://supabase.com/docs/reference/javascript/auth-exchangecodeforsession
+    const supabase = createRouteHandlerClient({ cookies });
+    supabase.auth.exchangeCodeForSession(code);
+  }
+
+  // go to home page of the app.
+  return NextResponse.redirect(url.origin);
 }
