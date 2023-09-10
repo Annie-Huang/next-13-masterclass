349452ea71cf6ccc13d21eeb9665dbc0f5b24fc0
Annie Huang <annie.hn@gmail.com>
2023-09-10 12:50:39 +1000

    If user is in the login page and already login, direct them to dashboard page.

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,6 +1,17 @@
 import Link from 'next/link';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function AuthLayout({ children }) {
+export default async function AuthLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+
+  // If user is in the login page and already login, direct them to dashboard page.
+  if (data.session) {
+    redirect('/');
+  }
+
   // Make it similar to <Navbar> component.
   // Serve http://localhost:3000/signup and http://localhost:3000/login
   return (
