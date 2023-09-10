44ebef005a2fafa878025e7e2c3e33e91cae3348
Annie Huang <annie.hn@gmail.com>
2023-09-10 11:30:25 +1000

    In the login page, call supabase.auth.signInWithPassword method

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,12 +1,19 @@
 'use client';
 
 import AuthForm from '@/app/(auth)/AuthForm';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 export default function Login() {
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
+    // console.log('user login', email, password);
 
-    console.log('user login', email, password);
+    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#client-side
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
   };
 
   return (
