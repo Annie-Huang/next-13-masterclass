e33bee7d7c7ef46d3e4807a8816a0ec6e96be9cb
Annie Huang <annie.hn@gmail.com>
2023-09-12 14:16:54 +1000

    Enable server action

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+// https://nextjs.org/docs/app/building-your-application/data-fetching/forms-and-mutations#how-server-actions-work
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
