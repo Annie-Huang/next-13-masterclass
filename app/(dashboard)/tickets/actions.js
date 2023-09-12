fc1a350dc1ce64dd2ba3eef569901765729a49cc
Annie Huang <annie.hn@gmail.com>
2023-09-12 14:39:46 +1000

    Connect to supabase and get session info from cookie

--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -1,9 +1,18 @@
 'use server';
 
+import { cookies } from 'next/headers';
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
+
 export async function addTicket(formData) {
   // console.log('hello from the server action');
 
   // Will create something like {title: 'xxx', body: 'yyy', priority: 'zzz'}
   const ticket = Object.fromEntries(formData);
   console.log('ticket=', ticket);
+
+  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#server-actions
+  const supabase = createServerActionClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 }
